6ea9dbb7c3d70b7807342d7dd85a558b443e90b7
Antoine Jean Fabre <pt52png@eduvaud.ch>
2025-05-19 11:17:08 +0200

    Insertion dans le cache de redis dans la route GetAll

--- Partie 1/backend/controllers/todo.controller.js
+++ Partie 1/backend/controllers/todo.controller.js
@@ -1,7 +1,7 @@
 const Todo = require('../models/todo.model.mongo');
 const User = require('../models/user.model.mongo');
 const mongoose = require('mongoose');
-
+const redisClient = require('../index.js');
 const TodoController = {
   createTodo: async (req, res) => {
     const user_id = req.sub;
@@ -39,13 +39,20 @@ const TodoController = {
   },
   getAllTodo: async (req, res) => {
     const user_id = req.sub;
-
+    const cacheKey = `todos:${user_id}`;
     try {
+      const cacheData = await redisClient.get(cacheKey);
+      if (cacheData) {
+        return res.status(200).json(cacheData);
+      }
       //const user = await User.findById(user_id).exec();
       //const user = await User.findById(user_id).sort({ date: 'asc' });
       const todos = await Todo.find({ user_id: user_id }, '_id text date completed user_id').exec();
       if (todos) {
-        console.log(todos[1]._id);
+        await redisClient.set();
+        await redisClient.set(cacheKey, JSON.stringify(todos), {
+          EX: 60 * 5
+        });
         return res.status(200).json(todos);
       }
       return res.status(404);
@@ -73,21 +80,17 @@ const TodoController = {
   editTodo: async (req, res) => {
     const user_id = req.sub;
     const id = req.params.id;
-
-    const todo = await Todo.findById({ _id: id }).updateOne({ completed: true });
-
-    /*
-    const query = { id: req.params.id, user_id: user_id };
     const data = req.body;
-    const result = await TodoModel.findOne({ where: query });
-    if (result) {
-      result.completed = data.completed ? data.completed : false;
-      result.text = data.text ? data.text : result.text;
-      result.date = data.date ? data.date : result.date;
-      await result
+    const todo = await Todo.findById({ _id: id });
+
+    if (todo) {
+      todo.completed = data.completed ? data.completed : false;
+      todo.text = data.text ? data.text : todo.text;
+      todo.date = data.date ? data.date : todo.date;
+      await todo
         .save()
         .then(() => {
-          return res.status(200).json(result);
+          return res.status(200).json(todo);
         })
         .catch((error) => {
           console.error('UPDATE TODO: ', error);
@@ -95,25 +98,23 @@ const TodoController = {
         });
     } else {
       return res.status(404);
-    }*/
+    }
   },
-  deleteTodo: (req, res) => {
+  deleteTodo: async (req, res) => {
     const user_id = req.sub;
     const todo_id = req.params.id;
-    const query = { id: todo_id, user_id: user_id };
-    console.log(req.params.id);
-    console.log(req.params);
-    Todo.deleteOne({ _id: todo_id });
-    /*TodoModel.destroy({
-      where: query
-    })
+    console.log('hrrlo', todo_id);
+
+    const query = { _id: todo_id, user_id: user_id };
+    console.log(query);
+    await Todo.findOneAndDelete(query)
       .then(() => {
-        return res.status(200).json({ id: todo_id });
+        return res.status(200).json({ _id: todo_id });
       })
       .catch((error) => {
         console.error('DELETE TODO: ', error);
         return res.status(500);
-      });*/
+      });
   },
   getSearchTodo: async (req, res) => {
     const user_id = req.sub;
