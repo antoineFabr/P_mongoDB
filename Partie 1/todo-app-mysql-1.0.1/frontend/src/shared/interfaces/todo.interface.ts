cdbc9ed1424a13052b2d7c7f2c152b9b3b283f0e
Antoine Jean Fabre <pt52png@eduvaud.ch>
2025-05-19 11:13:59 +0200

    Modification de tous les ids des todos dans le frontend

--- Partie 1/todo-app-mysql-1.0.1/frontend/src/shared/interfaces/todo.interface.ts
+++ Partie 1/todo-app-mysql-1.0.1/frontend/src/shared/interfaces/todo.interface.ts
@@ -1,5 +1,5 @@
 export interface Todo {
-  id: string;
+  _id: string;
   date: Date;
   text: string;
   completed: boolean;
--- Partie 1/todo-app-mysql-1.0.1/frontend/src/shared/interfaces/user.interface.ts
+++ Partie 1/todo-app-mysql-1.0.1/frontend/src/shared/interfaces/user.interface.ts
@@ -1,5 +1,5 @@
 export interface User {
-  id?: String;
+  id?: string;
   name: string;
   email: string;
   password: string;
